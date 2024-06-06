d22ef44b83b625366a5dd5633d86a8f24600be0d
Vadim <glebowadim@gmail.com>
2024-06-06 11:12:40 +0700

    Update sftp_to_ov.py

--- sftp_to_ov.py
+++ sftp_to_ov.py
@@ -1,9 +1,6 @@
 import sys
 import subprocess
 
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.txt'])
-
-
 import onevizion
 import argparse
 import pysftp
